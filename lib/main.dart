fe5df1e564f1535623a75edb51ef6b32e0d71474
Your Name <you@example.com>
2023-08-04 09:31:47 +0300

    test notes app

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,4 @@ void main() {
   runApp(const NotesApp());
 }
 
-class NotesApp extends StatelessWidget {
-  const NotesApp({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-
-    );
-  }
-}
