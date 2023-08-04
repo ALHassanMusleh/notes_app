84b734909d7be8d07ae16567ac85ccc1ebb4af69
Your Name <you@example.com>
2023-08-04 12:45:44 +0300

    create custom note item

--- lib/views/widgets/notes_view_body.dart
+++ lib/views/widgets/notes_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes_hive_app/views/widgets/custom_app_bar.dart';
+import 'package:notes_hive_app/views/widgets/custom_note_item.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({Key? key}) : super(key: key);
@@ -8,9 +9,11 @@ class NotesViewBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 35, horizontal: 25),
-      child:  Column(
+      child: Column(
         children: [
           CustomAppBar(),
+          CustomNoteItem(),
+
         ],
       ),
     );
