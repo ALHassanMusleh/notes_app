3d4d6c269496e2476ca14016035f2ea425f2b225
Your Name <you@example.com>
2023-08-10 18:27:54 +0300

    create edit note view

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes_hive_app/constant.dart';
+import 'package:notes_hive_app/views/widgets/custom_button.dart';
 import 'package:notes_hive_app/views/widgets/custom_text_field.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -9,22 +10,31 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0),
-      child: Column(
-        children: const [
-          SizedBox(
-            height: 35,
-          ),
-          CustomTextField(
-            hintText: 'Title',
-          ),
-          SizedBox(
-            height: 16,
-          ),
-          CustomTextField(
-            hintText: 'Content',
-            maxLines: 5,
-          ),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: const [
+            SizedBox(
+              height: 35,
+            ),
+            CustomTextField(
+              hintText: 'Title',
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            CustomTextField(
+              hintText: 'Content',
+              maxLines: 5,
+            ),
+            SizedBox(
+              height: 35,
+            ),
+            CustomButton(),
+            SizedBox(
+              height: 16,
+            ),
+          ],
+        ),
       ),
     );
   }
