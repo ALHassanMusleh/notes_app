1c726c738d14cc60f26d42151c2a4745c6f6b8c2
Your Name <you@example.com>
2023-08-12 22:38:35 +0300

    register type adapter

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:notes_hive_app/constant.dart';
+import 'package:notes_hive_app/models/note_model.dart';
 import 'package:notes_hive_app/views/notes_view.dart';
 
 void main() async {
   await Hive.initFlutter();
   await Hive.openBox(kNotesBox);
+  Hive.registerAdapter(NoteModelAdapter());
   runApp(const NotesApp());
 }
 
